67190a2568d27c8d3dcc1f25040696f3eefbe477
CatarineSales <catarinesales2013@gmail.com>
2025-10-27 22:34:19 -0300

    Organizando o código em arquivos

---
+++ script.js
@@ -0,0 +1,27 @@
+let BotaoAdd = document.getElementById('botao');
+let Input = document.getElementById('input');
+let Tarefas = document.getElementById('tarefas');
+let marcado = false;
+
+    BotaoAdd.addEventListener('click', function() {
+        if ( Input.value != ''){
+            var  tarefa = document.createElement('p');
+            Tarefas.appendChild(tarefa);
+            tarefa.innerText = '- ' + Input.value;
+            Input.value = '';
+            tarefa.addEventListener('click', function() {
+                if (marcado == false) {
+                    tarefa.style.textDecoration = 'Line-through'
+                    marcado = true;
+                }
+
+                else {
+                    tarefa.style.textDecoration = '';
+                    marcado = false;
+                }
+            })
+            tarefa.addEventListener( 'dblclick', function() {
+                Tarefas.removeChild(tarefa)
+            })
+        }
+    })
